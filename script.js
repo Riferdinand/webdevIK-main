e4b478bd5bb4bf86e5d8aec84c31eaecd83af0dc
Riferdinand <215410036@students.akakom.ac.id>
2023-01-07 10:55:30 +0700

    hh

---
+++ script.js
@@ -0,0 +1,32 @@
+window.addEventListener("scroll", function () {
+  var div = document.querySelector("navbar-hidden");
+  div.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// Collapsibles
+let coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+  // ketika class collapsible di click, jalankan:
+  coll[i].addEventListener("click", function () {
+    removeCollapseClass();
+    // tambahkan class active didalam tag class collapsible
+    this.classList.toggle("active");
+
+    // tambahkan style display didalam tag element selanjutnya
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// fungsi untuk menghilangkan collapse sebelumnya
+function removeCollapseClass() {
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].classList.remove("active");
+    coll[i].nextElementSibling.style.display = "none";
+  }
+}
